88b7c76a7d233d76b83ccc79cfa05da6f964f772
Dean <D193753@edurocwb.nl>
2014-11-21 11:36:42 +0100

    style

---
+++ website/header.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?>
+
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="banner">
+  <div class="titel">
+  <h1>Vlambeer</h1>
+  
+  </div>
+  
+  
+  <div class='menu'>
+  	 	 <p>Home</p>
+  		<p>Contact</p>
+ 		 <p>Store</p>
+  </div>
+</div>
+</body>
+
+</html>
