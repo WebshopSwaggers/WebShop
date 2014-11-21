c52f3f36be84b873cdd1b879a79b718ffb631131
Dean <D193753@edurocwb.nl>
2014-11-21 13:53:44 +0100

    syncen voor den jordy dan maar he

--- website/header.php
+++ website/header.php
@@ -4,25 +4,134 @@
 
 ?>
 
+<!DOCTYPE html> <!-- The new doctype -->
+
 <html>
 <head>
-  <link rel="stylesheet" href="style.css">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>Vlambeer</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
+
+
+
+
+
 <body>
-  <div class="banner">
-  <div class="titel">
-  <h1>Vlambeer</h1>
+ <section id="page"> <!-- Defining the #page section with the section tag -->
+	<header> <!-- Defining the header section of the page with the appropriate tag -->
+
+        <h1> Logo</h1>
+
+        <nav class="clear"> <!-- The nav link semantically marks your main site navigation -->
+
+            <ul>
+
+                <li><a href="#article1">News</a></li>
+                <li><a href="#article2">Contact</a></li>
+                <li><a href="#article3">Store</a></li>
+
+            </ul>
+
+        </nav>
+
+    </header>
+    
+      
+       
+        
+         
+           <!-- Article 1 start -->
+
+    
+
+    <article id="article1"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+<br><br><br><br>
+        <h2>News</h2>
+
+
+        <div class="articleBody clear">
+
+       <img src="img/header-510x186.png" alt="">
+            <p> Hier komen de laatste games van Vlambeer</p>
+
+
+        </div>
+
+    </article>
+
+    <!-- Article 1 end -->
+    
+    <article id="article2">
+    
+    
+        <h2>Contact</h2>
+
+    <h2> Hier komen de contact gegevens <br>
+    owner text <br>
+    friends of vlambeer  </h2>
+    
+    
+    <p><br>VLAMBEER IS A DUTCH INDEPENDENT GAME STUDIO made up of Rami Ismail and Jan Willem Nijman, bringing back arcade games since 1764.
+<br>Rami Ismail
+<br>Business and Development
+<br>rami@vlambeer.com
+<br>@tha_rami
+<br>Jan Willem Nijman
+<br>Game Design
+<br>jw@vlambeer.com
+<br>@jwaaaap
+<br>info@vlambeer.com | Neude 5, 3512 AD, Utrecht, the Netherlands | +31621206363
+<br>FRIENDS OF VLAMBEER
+<br>Roy Nathan de Groot
+<br>KOZILEK
+<br>Zach Gage
+<br>Greg Wohlwend
+<br>Kert Gartner
+<br>Phlogiston
+<br>Devolver Digital
+<br>Sparpweed
+<br> Mauer
+<br>The Poppenkast
+<br>Venus Patrol
+<br> Magazine
+<br>Pause music
+<br>Adam Atomic
+<br>Halfbot
+<br>Dutch Game Garden
+<br>Brandon Boyer
+<br>Bram Ruiter
+<br>Bit Collective
+<br>Aardbever
+ 
+ 
+ 
+ </p>
+    
+      
+
+        <div class="articleBody clear">
+    </article>
   
-  </div>
-  
-  
-  <div class='menu'>
-  	 	 <p>Home</p>
-  		<p>Contact</p>
- 		 <p>Store</p>
-  </div>
-</div>
-</body>
+         <footer> <!-- Marking the footer section -->
+
+
+            <a href="#" class="up">Go UP</a>
+
+        </footer>
+
+    </section> <!-- Closing the #page section -->
+
+    <!-- JavaScript Includes -->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script src="jquery.scrollTo-1.4.2/jquery.scrollTo-min.js"></script>
+    <script src="script.js"></script>
+
+    </body>
 
 </html>
