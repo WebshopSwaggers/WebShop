d630f47f5b817b5022726dd7c8c33a09a71e440e
jordynegen11 <jordy.visser@hotmail.com>
2014-11-24 16:01:28 +0100

    fix voor scroll

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -3,3 +3,15 @@ function getRandomDate(min, max){
 }
 document.getElementById('headerSlogan').innerHTML = "Bringing back arcade games since " + getRandomDate(1980, 2014);
 
+$(document).ready(function(){
+	/* This code is executed after the DOM has been completely loaded */
+
+	$('nav a,footer a.up').click(function(e){
+
+		// If a link has been clicked, scroll the page to the link's hash target:
+
+		$.scrollTo( this.hash || 0, 1500);
+		e.preventDefault();
+	});
+
+});
