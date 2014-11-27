bee8edfd65f051d757d14f4e6025a11c122bf15c
Slawek Pelka <D184045@edu.rocwb.nl>
2014-11-27 11:33:26 +0100

    music store

---
+++ website/store_music.php
@@ -0,0 +1,27 @@
+<?php
+$title = "Music";
+require 'templates/header.php';
+//DB require
+?>
+
+<div class="musicStoreContainer">
+	<div class="itemHolder">
+		<div class="itemPic">
+			<img src="assets\images\musicPlaceholder.png" alt="itemPlaceholder">
+		</div>
+		<strong>Item title</strong>
+		<br>
+		<i>Item description</i>
+	</div>
+
+	<div class="itemHolder">
+	  	<div class="itemPic">
+			<img src="assets\images\musicPlaceholder.png" alt="itemPlaceholder">
+		</div>
+		<strong>Item title</strong>
+		<br>
+		<i>Item description</i>
+	</div>
+</div>
+
+<?php require 'templates/footer.php';
