041112847bf0254b1cc056a6d3172f9453c8d87d
jordynegen11 <jordy.visser@hotmail.com>
2014-11-27 11:38:39 +0100

    header fix

--- website/store.php
+++ website/store.php
@@ -1,18 +1,18 @@
 <?php
-require 'templates/store_header.php';
+require 'templates/header.php';
 //DB require
 ?>
 
 
 <div class="vlambeergames">
-<h1>Games</h1>	
-	
+<h1>Games</h1>
+
 	<div class="gameslider">
 	 	<img src="assets/images/game%201.png" alt="">
-	 	
+
 	<img src="assets/images/game%202.png" alt="">
 	<img src="assets/images/game%203.png" alt="">
-	
+
 	</div>
 
 </div>
