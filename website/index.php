b36db221cf901e6b5d3954e00f5a6fec1dbcba95
Quincy Soeliman <qsoeliman@gmail.com>
2014-11-27 09:03:40 +0100

    Style

--- website/index.php
+++ website/index.php
@@ -3,22 +3,13 @@ require 'includes/config.php';
 require 'templates/header.php';
 ?>
 
-<div class="news">
-	<!-- Article 2 start -->
-	<article id="article2"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
-	<h2>News</h2>
+<!-- Article 2 start -->
+<article id="article2"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+<h2>News</h2>
 
-	<img src="assets/images/header-510x186.png" alt="Header">
-	<p> Hier komen de laatste games van Vlambeer</p>
-	
-	</article>
-	<!-- Article 2 end -->
-
-	<!-- Article 3 start -->
-	<article id="article3">
-
-	</article>
-	<!-- Article 3 end -->
-</div>
+<img src="assets/images/header-510x186.png" alt="Header">
+<p>Hier komen de laatste games van Vlambeer</p>
+</article>
+<!-- Article 2 end -->
 
 <?php require 'templates/footer.php'; ?>
