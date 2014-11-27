462f3c4c2fbe5aa1c618933291d83c61bcfefc88
Dean <D193753@edurocwb.nl>
2014-11-27 11:09:49 +0100

    store

--- website/store.php
+++ website/store.php
@@ -4,5 +4,19 @@ require 'templates/store_header.php';
 ?>
 
 
+<div class="vlambeergames">
+<h1>Games</h1>	
+	
+	<div class="gameslider">
+	 	<img src="assets/images/game%201.png" alt="">
+	 	
+	<img src="assets/images/game%202.png" alt="">
+	<img src="assets/images/game%203.png" alt="">
+	
+	</div>
+
+</div>
+
+
 
 <?php require 'templates/store_footer.php';
