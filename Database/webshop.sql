4066f51dcc742691c412fc4940c732101c88c458
jordynegen11 <jordy.visser@hotmail.com>
2014-11-28 09:13:43 +0100

    nieuwe db 2

--- Database/webshop.sql
+++ Database/webshop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50620
 File Encoding         : 65001
 
-Date: 2014-11-24 14:50:42
+Date: 2014-11-28 09:13:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,12 +23,17 @@ CREATE TABLE `cms_invoice_items` (
   `inv_item_id` int(11) NOT NULL DEFAULT '0',
   `inv_id` int(11) DEFAULT '0',
   `item_id` int(11) DEFAULT '0',
+  `count` int(11) DEFAULT '0',
   PRIMARY KEY (`inv_item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of cms_invoice_items
 -- ----------------------------
+INSERT INTO `cms_invoice_items` VALUES ('1', '1', '1', '3');
+INSERT INTO `cms_invoice_items` VALUES ('2', '1', '2', '5');
+INSERT INTO `cms_invoice_items` VALUES ('3', '2', '1', '2');
+INSERT INTO `cms_invoice_items` VALUES ('4', '1', '3', '2');
 
 -- ----------------------------
 -- Table structure for cms_invoice_templates
@@ -37,12 +42,16 @@ DROP TABLE IF EXISTS `cms_invoice_templates`;
 CREATE TABLE `cms_invoice_templates` (
   `inv_id` int(11) NOT NULL DEFAULT '0',
   `user_id` int(11) DEFAULT '0',
+  `start_date` varchar(255) DEFAULT '0',
+  `end_date` varchar(255) DEFAULT '0',
   PRIMARY KEY (`inv_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of cms_invoice_templates
 -- ----------------------------
+INSERT INTO `cms_invoice_templates` VALUES ('1', '1', '0', '0');
+INSERT INTO `cms_invoice_templates` VALUES ('2', '1', '0', '0');
 
 -- ----------------------------
 -- Table structure for cms_items
@@ -54,12 +63,16 @@ CREATE TABLE `cms_items` (
   `price` int(11) NOT NULL DEFAULT '0',
   `description` text NOT NULL,
   `image` varchar(255) DEFAULT NULL,
+  `catagory` varchar(100) DEFAULT 'games',
   PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of cms_items
 -- ----------------------------
+INSERT INTO `cms_items` VALUES ('1', 'COD MW2', '50', 'Call of duty MW2', 'http://upload.wikimedia.org/wikipedia/en/d/db/Modern_Warfare_2_cover.PNG', 'games');
+INSERT INTO `cms_items` VALUES ('2', 'SMB 3', '10', 'Super Mario Bros 3', 'http://upload.wikimedia.org/wikipedia/en/a/a5/Super_Mario_Bros._3_coverart.png', 'games');
+INSERT INTO `cms_items` VALUES ('3', 'BF 4', '60', 'muziekje', 'http://upload.wikimedia.org/wikipedia/en/e/ed/Battlefield_4.jpg', 'music');
 
 -- ----------------------------
 -- Table structure for cms_users
@@ -82,3 +95,4 @@ CREATE TABLE `cms_users` (
 -- ----------------------------
 -- Records of cms_users
 -- ----------------------------
+INSERT INTO `cms_users` VALUES ('1', 'jiojio@ge.nl', 'llllll', 'jordy', 'visser', 'jiojo', '989', '89', 'jiojjio', 'jiojio');
