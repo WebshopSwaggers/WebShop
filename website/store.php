3e6fcca92e72ba878831fbeeb0a39830e72c255a
Dean <D193753@edurocwb.nl>
2014-11-28 09:26:48 +0100

    store

--- website/store.php
+++ website/store.php
@@ -1,28 +1,34 @@
 <?php
-$title = "Games";
+$title = "games";
+require 'includes/config.php';
 require 'templates/header.php';
 //DB require
 ?>
-
-
 <div class="container">
-<div class="vlambeergames">
-<h1>Games</h1>
+<div class="musicStoreContainer">
 
-	<div class="gameslider">
-	 	<img src="assets/images/game%201.png" alt="">
+<?php
+$item = Item::getItems("games");
+$count = count($item);
+$i= 0;
+//while loop om het bedrag te bepalen
+for($count; $i < $count; $i++)
+{
 
-	<img src="assets/images/game%202.png" alt="">
-	<img src="assets/images/game%203.png" alt="">
+	echo'<div class="itemHolder">';
+		echo'<div class="itemPic">';
+			echo'<img src="'.$item[$i]['item_image'].'" alt="itemPlaceholder">';
+		echo'</div>';
+		echo'<strong>'.$item[$i]['item_name'].'</strong>';
+		echo'<br>';
+		echo'<i>'.$item[$i]['item_description'].'</i>';
+	echo'</div>';
 
-	</div>
-
-</div>
-
-
-
-<?php require 'templates/footer.php';
+}
 ?>
 
 </div>
 
+<?php require 'templates/footer.php';
+?>
+</div>
