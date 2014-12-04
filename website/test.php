150ad83d979b71c707b798fabaa36cd8a2f3602e
jordynegen11 <jordy.visser@hotmail.com>
2014-12-03 23:50:12 -0800

    test

--- website/test.php
+++ website/test.php
@@ -1,51 +1,24 @@
 <?php
-require 'includes/config.php';
+ini_Set("display_errors","On");
+$ip = "10.2.97.";
 
-//$userinv = Invoice::getUserInvoices(1);
+$i = 0;
 
-//$count = 0;
-//while($row = DB::fetch($userinv))
-//{
-//  $count++;
-  $invoice = addslashes($_GET['id']);
-  echo "<h1>Factuur #".$invoice."</h1>";
-
-  $inv = new Invoice($invoice);
-  $row = $inv->GetItemsForInvoice();
-
-  //variablen
-  $count = count($row);
-  $i= 0;
-  $bedrag = 0;
-
-  //while loop om het bedrag te bepalen
-  while($i < $count)
-  {
-    echo "Product: ".$row[$i]['item_name'];
-    echo "<br>";
-    echo "Beschrijving: ".$row[$i]['item_description'];
-    echo "<br>";
-    echo "Prijs: &euro;" . $row[$i]['item_price'];
-    echo "<br>";
-    echo "Aantal: ". $row[$i]['count'];
-    echo "<br>";
-    echo "<img style='height:100px; width:70px;' src='".$row[$i]['item_image']."'>";
-    echo "<br>";
-    echo "<br>";
-    $bedrag += $row[$i]['item_price'] * $row[$i]['count'];
-    $i++;
-  }
-
-  //rekensommen
-  $btw = $bedrag / 100 * 21;
-  $btwinc = $bedrag * 1.21;
-
-  //echo's
-  echo "Totale bedrag is: &euro;" . $bedrag;
-  echo "<br>";
-  echo "BTW (21%): &euro;" . $btw;
-  echo "<br>";
-  echo "Bedrag inclusief btw: &euro;" . $btwinc;
-
-
+for($i; $i < 20; $i++)
+{
+$_GET['ip'] = $ip.$i.":80";
+$array=explode(':',$_GET['ip']);
+$fp = @fsockopen($array[0], $array[1], $errno, $errstr,1);
+if($fp)
+{
+  $status = "<font color=\"green\">Online</font>";
+  fclose($fp); // hier sluiten omdat in het andere geval geen verbinding is opgebouwd
+}
+else
+{
+  $status = "<font color=\"red\">Offline</font>";
+}
+echo $ip.$i. " = " . $status."<br>";
+}
 ?>
+dd
