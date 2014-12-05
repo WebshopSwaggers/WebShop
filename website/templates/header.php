cbc52816a3108788adc9e50f108a04132052dc9c
Dean <D193753@edurocwb.nl>
2014-12-05 10:01:42 +0100

    slider

--- website/templates/header.php
+++ website/templates/header.php
@@ -127,6 +127,25 @@ echo'</nav>';
 
 
 <div class="container-img">
+<?php
+$item = Item::getItems("games");
+$count = count($item);
+$i= 0;
+for($count; $i < $count; $i++)
+{
+?>
+
+<div class="imgInfo">
+	<div class="imgInfoTitle">
+		<p><?php echo $item[$i]['item_name'] ?></p>
+	</div>
+	<div class="imgInfoDesc">
+		<p><?php echo $item[$i]['item_description'] ?></p>
+	</div>
+</div>
+<?php
+}
+?>
 <div class="image_slider">
 	<figure id="slideshow">
 
