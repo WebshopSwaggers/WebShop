c8d0705477d689cfbd301ed278817fae4c2a878b
Krytiv <qsoeliman@gmail.com>
2014-12-08 14:20:54 +0100

    Game sites

---
+++ website/gungodz.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Gun Godz";
+require 'includes/config.php';
+require 'templates/header.php';
+?>
+
+
+
+<?php 
+require 'templates/footer.php'; 
+?>
---
+++ website/serioussam.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Serious Sam";
+require 'includes/config.php';
+require 'templates/header.php';
+?>
+
+
+
+<?php 
+require 'templates/footer.php'; 
+?>
