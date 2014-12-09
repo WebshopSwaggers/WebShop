f3da68bd1c31cbd7f16667c13ec4fd5548cd4c98
Dean <D193753@edurocwb.nl>
2014-12-09 14:28:34 +0100

    sdf

--- website/games/serioussam.php
+++ website/games/serioussam.php
@@ -5,6 +5,80 @@ require '../templates/ss_header.php';
 ?>
 
 <div class="container">
+<?php
+$title = "Serious Sam";
+require '../includes/config.php';
+require '../templates/ss_header.php';
+?>
+
+<div class="container">
+<img src="/assets/images/ss_bomb.png" class="shakeimage" onMouseover="init(this);rattleimage()" onMouseout="stoprattle(this);top.focus()" onClick="top.focus()">
+
+
+
+<style>
+.shakeimage{
+position:relative
+}
+</style>
+<script language="JavaScript1.2">
+
+/*
+Shake image script (onMouseover)- 
+© Dynamic Drive (www.dynamicdrive.com)
+For full source code, usage terms, and 100's more DHTML scripts, visit http://dynamicdrive.com
+*/
+
+//configure shake degree (where larger # equals greater shake)
+var rector=3
+
+///////DONE EDITTING///////////
+var stopit=0 
+var a=1
+
+function init(which){
+stopit=0
+shake=which
+shake.style.left=0
+shake.style.top=0
+}
+
+function rattleimage(){
+if ((!document.all&&!document.getElementById)||stopit==1)
+return
+if (a==1){
+shake.style.top=parseInt(shake.style.top)+rector+"px"
+}
+else if (a==2){
+shake.style.left=parseInt(shake.style.left)+rector+"px"
+}
+else if (a==3){
+shake.style.top=parseInt(shake.style.top)-rector+"px"
+}
+else{
+shake.style.left=parseInt(shake.style.left)-rector+"px"
+}
+if (a<4)
+a++
+else
+a=1
+setTimeout("rattleimage()",50)
+}
+
+function stoprattle(which){
+stopit=1
+which.style.left=0
+which.style.top=0
+}
+
+</script>
+
+
+</div>
+
+<?php
+require '../templates/footer.php'; 
+?>
 
 </div>
 
