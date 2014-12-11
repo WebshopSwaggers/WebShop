2e38f4879c2b196cc8bbba142bf75c3e2cc32a1d
Dean <D193753@edurocwb.nl>
2014-12-11 11:06:56 +0100

    supercratebox swek

---
+++ website/games/headers/header_scb.php
@@ -0,0 +1,4 @@
+
+<div class="image_ss">
+  <img src="<?php echo $link; ?>/assets/images/supercratebox.gif" alt="Serious Sam">
+</div>
--- website/games/supercratebox.php
+++ website/games/supercratebox.php
@@ -1,11 +1,35 @@
 <?php
-$title = "Super Crate Box";
+$title = "Supercratebox ";
 $cat = "scb";
 require '../includes/config.php';
 require '../templates/header.php';
 ?>
 
+<div class="ss_container">
 
+
+<!-- Container -->
+<h1>About This Game</h1>
+<p>Super Crate Box is a freeware indie game released by Vlambeer on May 11, 2010.  It is an arcade action game. It is available on Steam, on the website for download, iOS, with multiplayer as Super Crate Box Versus on the Winnitron  and as Super Bread Box on the Commodore 64. </p>
+<br>
+<h2>Gameplay</h2>
+<p> Super Crate Box's core gameplay revolves around a 2D map. Enemies will continually spawn from the top of the map, and come in three varieties: small, large and flying. A crate is always available in a random location on the map, and picking up a crate gives you one of 14 weapons, selected at random. At all times, there is a fire pit at the bottom of the map. If an enemy reaches the bottom of the map and falls into the pit, it will respawn at the top of the map with the same health but moving considerably faster. Contact with an enemy results in immediate death.
+
+The goal of the game is to survive as long as possible by using the different weapons obtained from crates to kill enemies. The number of crates that the player picks up before dying constitutes as the score. By attaining certain numbers of crates in the game the player can unlock new weapons, game modes and cosmetic character skins.
+
+There is a leaderboard available on the official website that tracks high scores for the various different modes.
+
+The game supports joypad controls
+ </p>
+<br>
+<h2>Game Modes</h2>
+<ul>
+	<li>Normal - enemies spawn at a consistent rate</li>
+	<li>SFMT - more enemies spawn as more crates are collected (unlocked after scoring 40 on normal mode) </li>
+	<li>SAmbush - enemies spawn anywhere in the map (unlocked after scoring 20 on SFMT mode) </li>
+	
+</ul>
+</div>
 
 <?php
 require '../templates/footer.php';
