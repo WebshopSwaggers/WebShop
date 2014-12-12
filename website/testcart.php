f29c9be8d9a241a1a2ed2642c281417f91c00b28
Dean <D193753@edurocwb.nl>
2014-12-12 09:19:09 +0100

    testcart

---
+++ website/testcart.php
@@ -0,0 +1,27 @@
+<?php
+$title = "Home";
+require 'includes/config.php';
+require 'templates/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="" />
+</head>
+<div class="container">
+	
+	
+	
+</div>
+<body>
+    <script src=""></script>
+</body>
+</html>
+
+
+
+
+<?php require 'templates/footer.php'; ?></div>
