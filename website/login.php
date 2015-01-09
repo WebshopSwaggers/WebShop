9033726ad226fa8daa747d69c2810e75ab4baa80
Krytiv <qsoeliman@gmail.com>
2015-01-09 08:58:22 +0100

    Update

--- website/login.php
+++ website/login.php
@@ -1,1 +1,11 @@
-hier komt een login form
+<?php
+$title = "Home";
+require 'includes/config.php';
+require 'templates/header.php';
+?>
+
+<div class="container">
+	
+</div>
+
+<?php require 'templates/footer.php'; ?></div>
