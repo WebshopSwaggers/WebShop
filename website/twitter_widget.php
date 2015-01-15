4a3bf4775d0d599e3082e05abc24b45872368916
nick076 <nickdekoning@ziggo.nl>
2015-01-15 10:09:39 +0100

    twitter widget

--- website/twitter_widget.php
+++ website/twitter_widget.php
@@ -1,1 +1,2 @@
+<a class="twitter-timeline" href="https://twitter.com/Vlambeer" data-widget-id="540805784414986240">Tweets door @Vlambeer</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
