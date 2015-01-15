4c6869e34f4c9764a48f7a4010deb9c89307fec6
Dean <D193753@edurocwb.nl>
2015-01-15 12:32:54 +0100

    admin shizzle

---
+++ website/admin_user.php
@@ -0,0 +1,141 @@
+    <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css" />
+
+<?php
+
+require 'includes/config.php';
+
+
+$query = DB::query( "SELECT * FROM cms_users") or die(mysqli_error(DB::$con)); //welke tabel je wilt gebruiken uit de database
+
+	
+	
+	
+	if (isset($_GET['user_id'])) //De functie isset() kijkt of een variabele bestaat
+							//Je kan het dus gebruiken in formulieren of er op de submitknop gedrukt is (dan is de waarde van die knop 'gezet')
+		{
+		
+			$id = $_GET['user_id'];
+			$sql= "DELETE FROM cms_user WHERE user_id = '$id'";  //verwijderen via het id
+		
+			if (!$query = DB::query( $sql) ){  		//kijkt of het verwijderen goed is gegaan.
+				echo 'delete query is niet goed gegaan';
+				die();
+				
+			}
+		header ('location: admin_user.php');	//verwijderen is gelukt, dan blijft hij op zelfde pagina.
+		}
+	
+	
+	
+	
+	
+	
+	
+	if( isset($_POST['submit'])){				//isset $_POST voegt gegevens toe aan database  	
+		$email 			= 		$_POST['email']; //variabele aanmaken
+		$password 		= 	$_POST['password'];//variabele aanmaken
+		$firstname 			= 		$_POST['firstname'];//variabele aanmaken
+		$lastname 	= $_POST['lastname'];//variabele aanmaken
+		$street 	= $_POST['street'];//variabele aanmaken
+		$zip 	= $_POST['zip'];//variabele aanmaken
+		$number 	= $_POST['number'];//variabele aanmaken
+		$city 	= $_POST['city'];//variabele aanmaken
+		$country 	= $_POST['country'];//variabele aanmaken
+
+		
+		if (!$query 	= DB::query($con,"INSERT INTO cms_user(email, password, firstname, lastname) 
+											VALUES ('$email', '$password','$firstname', '$lastname')"))  //hier voegt hij toe waar het precies inmoet in database.
+		{
+			echo 'kan data niet toevoegen aan database'; //alshet niet lukt krijg je dit 
+		}else{
+			header('location: admin_user.php');   //blijft op zelfde pagina.
+		}
+	}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Pagina Users</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<div class="wrapper">
+		<h1>Admin Pagina Users</h1>
+
+			<table>
+				<thead>
+					<tr>
+						<th class="email">Email</th>
+						<th class="password">Password</th>
+						<th class="firstname">Firstname</th>
+						<th class="lastname">Lastname</th>
+						<th class="street">Street</th>
+						<th class="zip">Zip</th>
+						<th class="number">Number</th>
+						<th class="city">City</th>
+						<th class="country">Country</th>
+
+						<th class="bewerk">Bewerken</th>
+						<th class="verwijder">Verwijder</th>
+					</tr>	
+				</thead>
+
+				<tbody>
+					<tr>
+						<?php
+							while($row = mysqli_fetch_assoc($query)){
+								echo '<tr>';
+								echo '<td>' . $row['email'] . '</td>';
+								echo '<td>' . $row['password'] . '</td>'; 
+								echo '<td>' . $row['firstname'] . '</td>';  
+								echo '<td>' . $row['lastname'] . '</td>'; 
+								echo '<td>' . $row['street'] . '</td>'; 
+								echo '<td>' . $row['zip'] . '</td>'; 
+								echo '<td>' . $row['number'] . '</td>'; 
+								echo '<td>' . $row['city'] . '</td>'; 
+								echo '<td>' . $row['country'] . '</td>'; 
+
+								echo '<td> <a href="admin_user_edit.php?user_id='. $row['user_id'].'"> Bewerk </a></td>';
+								echo '<td> <a href="admin_user.php?id='. $row['user_id'].'"> X </a></td>';
+								echo '</tr>';
+							}
+						?>
+
+					</tr>
+				</tbody>
+			</table>
+
+			<form action="" method="POST">
+				<label for="email">Email</label>
+				<input type="email" name="email" id="email" required>
+
+				<br>
+
+				<label for="password">Password</label>
+				<input type="text" name="password" id="password" required>
+
+				<br>
+
+				<label for="firstname">First Name</label>
+				<input type="text" name="firstname" id="firstname" required>
+
+				<br>
+
+				<label for="lastname">Last Name</label>
+				<input type="text" name="lastname" id="lastname" required>
+
+				<input name="submit" type="submit" value="toevoegen">
+			</form>
+
+
+	</div>
+</body>
+</html>	
+
+
---
+++ website/admin_user_edit.php
@@ -0,0 +1,70 @@
+<?php
+
+require 'includes/config.php';
+
+
+$query = DB::query( "SELECT * FROM cms_users") or die(mysqli_error(DB::$con)); //welke tabel je wilt gebruiken uit de database
+
+
+if(isset($_GET['user_id'])){
+
+$id = $_GET['user_id'];
+$sql = DB::query("SELECT * FROM cms_users WHERE user_id = $id") or die(mysqli_error(DB::$con));
+$row = DB::fetch_assoc($sql);
+
+
+
+
+} else { header("location: admin_user.php");
+}
+
+if (isset($_POST['submit'])){
+	$email 			= Security($con, $_POST['email']);
+	$password 		= Security($con, $_POST['password']);
+	$firstname 			= Security($con, $_POST['firstname']);
+	$lastname 	= Security($con, $_POST['lastname']);
+
+$sql = "UPDATE cms_users SET email = '$email', password = '$password', firstname = '$firstname', lastname = '$lastname'WHERE user_id = '$id'" or die(mysqli_error(DB::$con));
+
+
+if (!$query = DB::query($sql)){
+
+	echo 'kan gegevens niet updaten';
+	die();
+}
+
+$msg = urlencode('User is succesvol upgedate');
+header('location: admin_user_edit.php?msg='. $msg);
+}	
+?>
+
+<h1>Edit Pagina</h1>
+
+<?php
+
+if (isset($_GET['msg'])){
+	echo '<p>' . $_GET ['msg']. '</p>';
+}
+?>
+
+<form action="admin_user_edit.php" method="POST">
+	<label for="email">email</label>
+	<input type="email"  value='<?php echo $row['email'];?>'name="email" id="email" required>
+
+	<br>
+
+	<label for="uitgever">uitgever</label>
+	<input type="text" value='<?php echo $row['password'];?>'name="password" id="password" required>
+
+	<br>
+
+	<label for="firstname">firstname</label>
+	<input  type="text" value='<?php echo $row['firstname'];?>'name="firstname" id="firstname" required>
+	<input type="hidden" value='<?php echo $row['user_id'];?>'>
+	<br>
+
+	<label for="lastname">lastname</label>
+	<input type="text" value='<?php echo $row['lastname'];?>'name="lastname" id="lastname" required>
+				
+	<input name="submit" type="submit" value="bewerken">
+</form>
