017a9bbabe0fff4add0ad34346dd0577ea499f8b
jordynegen11 <jordy.visser@hotmail.com>
2015-01-15 12:18:41 +0100

    zoooi

--- website/templates/header.php
+++ website/templates/header.php
@@ -10,9 +10,10 @@
 </head>
 <body>
 <div class="header">
-    <!-- <div class="header_animation">
-        <img src="assets/images/waves.gif" alt="">
-    </div> -->
+  <div class="header_animation">
+        <img src="assets/images/waves.gif" style="
+        width: 937px;" alt="">
+    </div>
 </div>
 
 <div class="navbar"></div>
@@ -25,37 +26,4 @@
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     </ul>
-</div>
-<div class="image_slider">
-    <div class="mask">
-        <ul class="images">
-          <li>
-             <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/270px-The_Earth_seen_from_Apollo_17.jpg" />
-          </li>
-          <li>
-             <img width="921" src="http://www.mallorcaweb.net/masm/Planetas/Jupiter.jpg" />
-          </li>
-           <li>
-             <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/280px-FullMoon2010.jpg" />
-          </li>
-          <li>
-             <img width="921" src="http://static.ddmcdn.com/gif/sun-update-1.jpg" />
-          </li>
-          <li>
-             <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Neptune.jpg/240px-Neptune.jpg" />
-          </li>
-       </ul>
-   </div>
-
-   <ul class="triggers">
-       <li></li>
-       <li></li>
-       <li></li>
-       <li></li>
-       <li></li>
-    </ul>
-
-    <span class="control prev"></span>
-    <span class="control next"></span>
-
 </div>
