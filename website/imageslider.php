f7f6d64783c6357adb56ccf8bba92e4431df84d5
Krytiv <qsoeliman@gmail.com>
2015-01-15 09:40:50 +0100

    Slider

--- website/imageslider.php
+++ website/imageslider.php
@@ -6,34 +6,35 @@
 <div class="mask">
    <ul class="images">
       <li>
-         <img width="270" src="http://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/270px-The_Earth_seen_from_Apollo_17.jpg" />
+         <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/270px-The_Earth_seen_from_Apollo_17.jpg" />
       </li>
       <li>
-         <img width="270" src="http://www.mallorcaweb.net/masm/Planetas/Jupiter.jpg" />
+         <img width="921" src="http://www.mallorcaweb.net/masm/Planetas/Jupiter.jpg" />
       </li>
        <li>
-         <img width="270" src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/280px-FullMoon2010.jpg" />
+         <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/280px-FullMoon2010.jpg" />
       </li>
       <li>
-         <img width="270" src="http://static.ddmcdn.com/gif/sun-update-1.jpg" />
+         <img width="921" src="http://static.ddmcdn.com/gif/sun-update-1.jpg" />
       </li>
       <li>
-         <img width="270" src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Neptune.jpg/240px-Neptune.jpg" />
+         <img width="921" src="http://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Neptune.jpg/240px-Neptune.jpg" />
       </li>
    </ul>
 </div>
 
 <ul class="triggers">
-   <li>0</li>
    <li>1</li>
    <li>2</li>
    <li>3</li>
    <li>4</li>
+   <li>5</li>
 </ul>
 
 <span class="control prev">Prev</span>
 <span class="control next">Next</span>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="assets/js/imageSlider.js"></script>
 
 </body>
