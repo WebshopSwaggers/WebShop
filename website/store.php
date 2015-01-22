48c522989d70a00b371365a4c1d5ac08848ece8e
Slawek Pelka <D184045@edu.rocwb.nl>
2015-01-22 10:08:06 +0100

    ff die shit itemholder fixen + new layout

--- website/store.php
+++ website/store.php
@@ -66,27 +66,30 @@ require 'templates/header.php';
 
 	for($count; $i < $count; $i++)
 	{
-		echo'<div class="itemHolder">';
-		echo'<div class="itemPic">';
-		echo'<img class="bottom" src="'.$item[$i]['item_image'].'" alt="itemPlaceholder">';
-		?>
-		<img class="top" onclick="realTime('<?php echo $item[$i]['item_id']; ?>', '<?php echo $dir; ?>');" src="assets/images/buyme.png">
+
+			echo'<div class="itemHolder">';
+				echo'<div class="itemPic">';
+					echo'<img class="bottom" src="'.$item[$i]['item_image'].'" alt="itemPlaceholder">';
+			?>
+		<img class="top" onclick="realTime('<?php echo $item[$i]['item_id'];?>', '<?php echo $dir; ?>');" src="assets/images/buyme.png">
 		<?php
 		echo'</div>';
 
-		echo'<div class="itemTitle">';
-		echo'<strong>'.$item[$i]['item_name'].'</strong>';
-		echo'<br>';
-		echo'</div>';
+				echo'<div class="itemTitle">';
+					echo'<strong>'.$item[$i]['item_name'].'</strong>';
+				echo'<br>';
+				echo'</div>';
 
-		echo'<div class="itemDesc">';
-		echo'<p>'.$item[$i]['item_description'].'</p>';
-		echo'</div>';
+				echo'<div class="itemDesc">';
+						echo'<p>'.$item[$i]['item_description'].'</p>';
+				echo'</div>';
 
-		echo'<div class="itemPrice">';
-		echo'<i>Price: &euro;'.$item[$i]['item_price'].'</i>';
-		echo'</div>';
-		echo'</div>';
+				echo'<div class="itemPrice">';
+					echo'<i>Price: &euro;'.$item[$i]['item_price'].'</i>';
+				echo'</div>';
+			echo'</div>';
+			?>
+				<?php
 	}
 	?>
 	</div>
