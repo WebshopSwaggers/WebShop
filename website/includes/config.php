d6cc23d11c0acdfe71d00f280b98bd95f6ebdb6b
Jessepeffer <jesse1995peffer@hotmail.com>
2015-01-22 09:32:54 +0100

    registratie

--- website/includes/config.php
+++ website/includes/config.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 session_start();
 
 $url = "http://localhost/";
-$dir = "/git/WebShop/website";
+$dir = "/GitHub/WebShop/website";
 $link = $url.$dir;
 
 require("classes/class.mysqli.php");
